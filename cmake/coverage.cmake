baba779a16fc075ce05a68a05107c089bc791c41
cuzperf <cuzperf@outlook.com>
2026-07-10 12:01:34 +0800

    更新 lcov 忽略覆盖度方式

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -19,15 +19,12 @@ endif()
 
 set(THIRD_PARTY_FILES "*/third_party/*")
 set(TEST_FILES "*/tests/*")
-set(IGNORE_FILES1 "*/src/lisp_main.c")
-set(IGNORE_FILES2 "*/src/lisp_repl.c")
-set(IGNORE_FILES3 "*/src/lisp_dump.c")
 
 # 检测 lcov 版本，动态设置参数
 execute_process(
-    COMMAND lcov --version
-    OUTPUT_VARIABLE LCOV_VERSION_OUTPUT
-    OUTPUT_STRIP_TRAILING_WHITESPACE
+  COMMAND lcov --version
+  OUTPUT_VARIABLE LCOV_VERSION_OUTPUT
+  OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 # 提取版本号（例如 "lcov: LCOV version 1.16"）
@@ -44,7 +41,7 @@ message(STATUS " LCOV_IGNORE_ERRORS=${LCOV_IGNORE_ERRORS}")
 
 # Generate coverage data
 add_custom_target(coverage
-  COMMAND lcov ${LCOV_IGNORE_ERRORS} --directory . --capture --exclude "${THIRD_PARTY_FILES}" --exclude "${TEST_FILES}" --exclude "${IGNORE_FILES1}" --exclude "${IGNORE_FILES2}" --exclude "${IGNORE_FILES3}" --output-file coverageall.info
+  COMMAND lcov ${LCOV_IGNORE_ERRORS} --directory . --capture --exclude "${THIRD_PARTY_FILES}" --exclude "${TEST_FILES}" --output-file coverageall.info
   COMMAND lcov ${LCOV_IGNORE_ERRORS} --remove coverageall.info ${LCOV_REMOVE_EXTRA} --output-file coverage.info
   COMMAND genhtml coverage.info --output-directory coverage
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
