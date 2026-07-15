eda690f536cec20641426092df85fbe63a665228
cuzperf <cuzperf@outlook.com>
2026-07-15 17:15:57 +0800

    add more UT for corner case

---
+++ tests/unit_test/test_cflisp_no_system.cpp
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+#include "test_utils.h"
+#include "stdio.h"
+#include <stdlib.h>
+
+class LispTestNoSystem : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    cf_lisp_init();
+  }
+};
+
+TEST_F(LispTestNoSystem, test1)
+{
+    value_t res = cl_eval_string("((fn () (+ 1 2)))");
+    EXPECT_EQ(cf_num_val(res), 3);
+}
+
+static void noEnoughArgs()
+{
+    cl_eval_string("((fn (x y) (+ x y)) 1)");
+}
+TEST_F(LispTestNoSystem, HandleExit_1)
+{
+    EXPECT_EXIT(noEnoughArgs(), ::testing::ExitedWithCode(1), "");
+}
